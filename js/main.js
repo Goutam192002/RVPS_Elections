57ef4202db15984d0dbd702655a5deb3667332be
Seervi Company <goutambseervi@gmail.com>
2018-07-19 18:12:39 +0530

    RVPS Election Application

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function loadStudentForm() {
+let student_details=document.getElementById("student-details-section");
+if(student_details.style.display==='none')
+{
+    student_details.style.display='flex';
+}
+else
+{
+    student_details.style.display='none';
+}
+}
+
