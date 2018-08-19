8feae963b82fb9495a20ffb7a61b6ef6e42a725d
Seervi Company <goutambseervi@gmail.com>
2018-08-19 09:02:53 +0530

    Update

--- js/admin_script.js
+++ js/admin_script.js
@@ -11,6 +11,19 @@ $(document).ready(function () {
                 dataType: "json",
                 success: function (response) {
                         console.log(response);
+                        document.getElementById("admin_details_name").append(response.admin_name);
+                        //document.getElementById("admin_details_password").type="password";
+                        document.getElementById("admin_details_password").append(convertToPassword(response.admin_password));
+                        document.getElementById("admin_details_mobileno").append(response.admin_mobile_no);
+                        document.getElementById("admin_details_email").append(response.admin_email);
                 }
             })
         });
+function convertToPassword(val) {
+   let result="";
+    for(let i=0;i<val.length;i++)
+    {
+        result+="*";
+    }
+    return result;
+}
